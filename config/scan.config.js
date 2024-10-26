2c0474975c079579d11e1e62f37ffe03ce7356d1
kratoon <ondrej@kratochvil.dev>
2024-10-26 18:56:10 +0200

    Add github action to validate PR

--- config/scan.config.js
+++ config/scan.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  "*.cls": (filenames) =>
-    `sf scanner run --engine "pmd" --pmdconfig config/pmd.xml --severity-threshold 3 -t ${filenames.join(
-      ",",
-    )}`,
+    '*.cls': (filenames) => `sf scanner run --engine "pmd" --pmdconfig config/pmd.xml --severity-threshold 3 -t ${filenames.join(',')}`,
 };
